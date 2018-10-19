d17a36e3bcc5fdfb517b38dfb038b7d0cd77f759
David Konsumer <konsumer@jetboystudio.com>
2018-10-18 18:11:15 -0700

    demo shows how compoentns could work

--- src/index.js
+++ src/index.js
@@ -11,14 +11,19 @@ import {
   Sphere
 } from './react-babylon'
 
+// this illustrates that stuff can load form different components
+const DemoScene = () => (
+  <Scene>
+    <Sphere name='player' />
+    <FreeCamera name='camera1' y={5} z={-10} target='player' />
+    <HemisphericLight name='light1' intensity={0.7} direction='up' />
+  </Scene>
+)
+
 function Demo () {
   return (
     <Engine>
-      <Scene>
-        <Sphere name='player' />
-        <FreeCamera name='camera1' y={5} z={-10} target='player' />
-        <HemisphericLight name='light1' intensity={0.7} direction='up' />
-      </Scene>
+      <DemoScene />
     </Engine>
   )
 }
